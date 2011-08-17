0ef18e64bb9412a45fbca22b61854d7cdfd19df0
Jakob Færch <jakob@faerch-net.dk>
2011-08-17 15:45:43 +0200

    Login virker endnu ikke

--- src/main/java/com/trifork/pip/scraper/ObservationPoster.java
+++ src/main/java/com/trifork/pip/scraper/ObservationPoster.java
@@ -1,23 +1,60 @@
 package com.trifork.pip.scraper;
 
+import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.*;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
 import java.io.IOException;
 
 public class ObservationPoster {
-    private static final String URL = "http://www.fugleognatur.dk/indtastobs.aspx";
+    private final WebClient webClient = new WebClient();
+    public static final String BASE_URL = "http://www.fugleognatur.dk";
+    private static final String LOGIN_URL = BASE_URL + "/login.asp";
+    private static final String POST_OBSERVATION_URL = BASE_URL + "/indtastobs.aspx";
 
-    public void postObservation() {
-        final WebClient webClient = new WebClient();
-        final HtmlPage page;
+    public ObservationPoster() {
+        webClient.setRedirectEnabled(true);
+        webClient.setCssEnabled(false);
+        webClient.setActiveXNative(false);
+        login();
+    }
+
+    private void login() {
         try {
-            page = webClient.getPage(URL);
+            HtmlPage loginPage = webClient.getPage(LOGIN_URL);
+
+            FileUtils.writeStringToFile(new File("/Users/jakob/tmp/beforelogin.html"), loginPage.asXml());
+
+            HtmlForm loginForm = loginPage.getFormByName("frmLogin2");
+            HtmlInput username = loginForm.getInputByName("box1");
+            HtmlInput password = loginForm.getInputByName("box2");
+            HtmlButtonInput submitBtn = loginForm.getInputByName("btnSubmit2");
+
+            username.setValueAttribute("jakobfaerch");
+            password.setValueAttribute("46n12b");
+
+
+            HtmlPage afterLogin = submitBtn.click();
+
+            FileUtils.writeStringToFile(new File("/Users/jakob/tmp/afterlogin.html"), afterLogin.asXml());
+
+            webClient.closeAllWindows();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        System.out.println(page.asText());
+    public void postObservation() {
+        try {
+            final HtmlPage page;
+            page = webClient.getPage(POST_OBSERVATION_URL);
+
+            FileUtils.writeStringToFile(new File("/Users/jakob/tmp/observationpage.html"), page.asXml());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         webClient.closeAllWindows();
     }
